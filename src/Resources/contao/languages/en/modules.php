b5f67956f4fda15507b78d70a2ac8ffbd44ec01b
jonny <jonnyspitzner@googlemail.com>
2018-03-05 17:50:53 +0100

    fix modul description

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,5 +1,7 @@
 <?php
+
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['cookieconsent'] = array('Cookieconsent','The Cookie popup.');
+$GLOBALS['TL_LANG']['MOD']['cookieconsent'] = array('Cookieconsent', 'The Cookie popup.');
+$GLOBALS['TL_LANG']['FMD']['cookieconsent'] = array('Cookieconsent', 'The Cookie popup.');
